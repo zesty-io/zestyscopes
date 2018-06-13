f078deb66ce19a3e766493239cc81478d02d75a0
Simon Prickett <simon.prickett@zesty.io>
2018-06-12 22:30:31 -0700

    Adds update for sign dates intent.

--- alexa/index.js
+++ alexa/index.js
@@ -1,111 +1,131 @@
-'use strict';
+'use strict'
 
-// zestyscopes Alexa skill.
+const Alexa = require('ask-sdk')
+const Request = require('request')
+const ZESTY_API_BASE = 'http://zestyscopes.zesty.site/-/custom'
 
-const Alexa = require('ask-sdk');
-
-let skill;
+let skill
 
 exports.handler = async function (event, context) {
-  console.log(`REQUEST++++${JSON.stringify(event)}`);
   if (!skill) {
     skill = Alexa.SkillBuilders.custom()
       .addRequestHandlers(
         LaunchRequestHandler,
-        HelloWorldIntentHandler,
+        StarSignDatesIntentHandler,
         HelpIntentHandler,
         CancelAndStopIntentHandler,
         SessionEndedRequestHandler,
       )
       .addErrorHandlers(ErrorHandler)
-      .create();
+      .create()
   }
   
-  return skill.invoke(event,context);
+  return skill.invoke(event,context)
 }
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest'
     },
     handle(handlerInput) {
-        const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+        const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!'
 
         return handlerInput.responseBuilder
             .speak(speechText)
             .reprompt(speechText)
             .withSimpleCard('Hello World', speechText)
-            .getResponse();
+            .getResponse()
     }
-};
+}
 
-const HelloWorldIntentHandler = {
+const StarSignDatesIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+            && handlerInput.requestEnvelope.request.intent.name === 'StarSignDatesIntent'
     },
     handle(handlerInput) {
-        const speechText = 'Hello World!';
+        return new Promise((resolve, reject) => {
+            const starSign = handlerInput.requestEnvelope.request.intent.slots.star_sign.resolutions.resolutionsPerAuthority[0].values[0].value.name
 
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .withSimpleCard('Hello World', speechText)
-            .getResponse();
+            Request(
+                {
+                    url: `${ZESTY_API_BASE}/starsign.json?sign=${starSign}`,
+                    json: true
+                }, 
+                (error, response, starSignInfoArr) => {
+                    let speechText
+
+                    if (response.statusCode !== 200 || error) {
+                        reject()
+                    } else {
+                        const starSignInfo = starSignInfoArr[0]
+                        speechText = `People with birthdays between ${starSignInfo.startDate} and ${starSignInfo.endDate} have ${starSign} as their sign.`
+        
+                        resolve(
+                            handlerInput.responseBuilder
+                                .speak(speechText)
+                                .withSimpleCard(`Dates for ${starSign}`, speechText)
+                                .getResponse()  
+                        )    
+                    }
+                }
+            )
+        })
     }
-};
+}
 
 const HelpIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent'
     },
     handle(handlerInput) {
-        const speechText = 'You can say hello to me!';
+        const speechText = 'You can say hello to me!'
 
         return handlerInput.responseBuilder
             .speak(speechText)
             .reprompt(speechText)
             .withSimpleCard('Hello World', speechText)
-            .getResponse();
+            .getResponse()
     }
-};
+}
 
 const CancelAndStopIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
             && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
-                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent')
     },
     handle(handlerInput) {
-        const speechText = 'Goodbye!';
+        const speechText = 'Goodbye!'
 
         return handlerInput.responseBuilder
             .speak(speechText)
             .withSimpleCard('Hello World', speechText)
-            .getResponse();
+            .getResponse()
     }
-};
+}
 
 const SessionEndedRequestHandler = {
     canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest'
     },
     handle(handlerInput) {
         //any cleanup logic goes here
-        return handlerInput.responseBuilder.getResponse();
+        return handlerInput.responseBuilder.getResponse()
     }
-};
+}
 
 const ErrorHandler = {
     canHandle() {
-      return true;
+      return true
     },
     handle(handlerInput, error) {
-      console.log("Error handled: "+error.message+"}");
+      console.log("Error handled: "+error.message+"}")
   
       return handlerInput.responseBuilder
         .speak('Sorry, I can\'t understand the command. Please say again.')
         .reprompt('Sorry, I can\'t understand the command. Please say again.')
-        .getResponse();
+        .getResponse()
     },
-  };
+  }
